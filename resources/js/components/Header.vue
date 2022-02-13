eae0a2be90b5cbf4b80bb8f48df4af5d59be0e34
Yu4iy <yurimohyliuk@gmail.com>
2022-02-13 18:11:48 +0100

    ADD: Header

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,102 @@
+<template>
+	<header>
+		<div class="container">
+			<img src="../assets/dc-logo.png" alt="">
+			<ul>
+				<li  v-for = "(link,index) in links"
+				:key="index"
+				>
+				<a :class="{active: index === 1}" href="">{{link}}</a>
+
+				</li>
+				
+			</ul>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name:'Header',
+	data() {
+	return {
+		links: 
+			[
+				"characters",
+				"comics",
+				"movies",
+				"tv",
+				"games",
+				"collectibles",
+				"videos",
+				"fans",
+				"news",
+				"shop",
+			],
+		}
+	}
+}
+
+</script>
+
+<style lang = 'scss' scoped >
+@import '../styles/variables';
+
+	header{
+		position: relative;
+		
+		& > div{
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			
+			img{
+				max-width: 80px;
+			}
+		}
+
+		ul{
+			list-style: none;
+			display: flex;
+
+			li{
+				margin-right: 30px;
+				position: relative;
+				text-transform: uppercase;
+				height: 100px;
+				display: flex;
+				align-items: center;
+			
+				
+				&:last-child{
+					margin-right: 0;
+
+				}
+					
+				
+				a{
+					font-size: 0.7rem;
+					font-weight: 700;
+					color: $brownColor;
+					&:hover{
+						color: $brandColor;
+					}
+				
+				} 
+			}
+		}
+		
+
+	}
+	
+	.active::after{
+						content: '';
+						width: 100%;
+						height: 4px;
+						background-color: $brandColor;
+						display: block;	
+						position: absolute;
+						bottom: 0;
+						}
+	
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,21 +1,31 @@
 
 <template>
+<div>
+	<!-- HEADER -->
+	<Header/>
+
+	<!-- MAIN  -->
+	<main>Main</main>
+
+	<!-- FOOTER -->
 	
-	<main>
-	<h1>TEST  VUE</h1>
-	</main>
+</div>
 
 </template>
 
 <script>
+import Header from '../components/Header.vue'
 export default {
 name:'App',
 components:{
+	Header,
 },
 
 }
 </script>
 
 <style lang="scss">
+@import '../styles/variables';
+@import '../styles/globals';
 
 </style>
